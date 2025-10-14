a809464ba40bce8e04ed5ac6852f68fb614dc275
Lwin Min Thein <lwinthein679@gmail.com>
2025-10-14 09:16:46 +0630

    add dropdown

--- src/features/sales&payment/pages/Sales.jsx
+++ src/features/sales&payment/pages/Sales.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import SearchBar from '../../../component/common/SearchBar'
+import Dropdown from '../../../component/common/Dropdown'
 
 const Sales = () => {
+      const list = ["CARD", "CASH", "E-Money"];
+      const Dref=useRef();
+
+const HandleChange=(value)=>{console.log(value);
+}
+    
+    
   return (
     <div>
 
         <SearchBar placeholder={"Search"} className={`w-fit`}/>
+        <Dropdown onChange={HandleChange} placeholder={"Payment"} items={list} ref={Dref}/>
 
     </div>
   )
